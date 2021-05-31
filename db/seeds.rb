0cd0a8d2f88e6ac239f32c919fb2358c3adebdc0
Marius Gavini <marius@gavini.fr>
2021-05-31 16:32:47 +0200

    seed user*3 hero*5 thanks to api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'faker'
 
+puts 'Creating 3 fake users'
+3.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+    )
+  user.save!
+end
+
+heros = [70, 322, 333, 332, 505]
+
+url = "https://superheroapi.com/api/6256070421085368"
+
+
+heros.each do |hero|
+hero_json = JSON.parse("#{url}/#{hero}/biography")
+hero_category = JSON.parse("#{url}/#{hero}/work")
+hero_picture = JSON.parse("#{url}/#{hero}/image")
+hero = Hero.new(
+  name: hero_json['name']
+  category: hero_category['occupation']
+  user_id: [1, 2, 3].sample
+  fullname: hero_json['full-name']
+  publisher: hero_json['publisher']
+  alignment: hero_json['alignment']
+  picture: hero_picture['url']
+  )
+hero.save!
+end
