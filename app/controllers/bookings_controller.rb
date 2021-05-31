10c865800694d7530386b2603b8c35a53e83003f
Laetitia de Borde <laetitiadeborde@gmail.com>
2021-05-31 15:21:17 +0200

    add routes, bookings & heroes controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,37 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :hero_id, :status)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :heroes, only: [:index, :new, :show, :create, :destroy]
+  resources :bookings, only: [:index, :new, :show, :create, :destroy]
 
 end
