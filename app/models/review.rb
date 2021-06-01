e5d30ffca533c8ecd10056208c01b9344faae365
Laetitia de Borde <laetitiadeborde@gmail.com>
2021-06-01 17:05:27 +0200

    add user for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :hero
+  belongs_to :user
 
   validates :rating, inclusion: { in: (0..10),  message: "Your rating should be between 1 and 10" }
   validates :content, presence: true
